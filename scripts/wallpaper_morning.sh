eef9a5981b4746d455b3569cd03810d3606de5ba
Kerem <ahkekaya@gmail.com>
2025-11-03 12:46:59 +0300

    Update wallpaper scripts to version 0.2.6 and 0.2.7; change file permissions

--- scripts/wallpaper_morning.sh
+++ scripts/wallpaper_morning.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-VERSION= "0.2.5"
+VERSION= "0.2.7"
 IMAGE="/opt/homebrew/Cellar/riset/$VERSION/share/riset/assets/morning.jpg"
 osascript <<EOF
 tell application "System Events"
